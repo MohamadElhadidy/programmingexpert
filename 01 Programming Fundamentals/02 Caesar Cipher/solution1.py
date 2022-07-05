3387be88a00d7803b5c0f0a5c2b6d8b7bcecad4e
unknown <elhadidy.384@gmail.com>
2022-07-05 15:52:21 +0200

    Caesar Cipher

--- 01 Programming Fundamentals/02 Caesar Cipher/solution1.py
+++ 01 Programming Fundamentals/02 Caesar Cipher/solution1.py
@@ -2,7 +2,7 @@ def caesar_cipher(string, offset):
     new_string = []
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    for key, char in enumerate(string):
+    for char in string:
         for key2, char2 in enumerate(alphabet):
             if char == char2:
                 position = key2 - offset
